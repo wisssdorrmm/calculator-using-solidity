2f5b35931a1f2435595b5f7156bff91dae584115
wisssdorrmm <wisdomudoudo24@gmail.com>
2025-11-09 08:55:17 +0100

    wallet integration

--- .deps/time.sol
+++ .deps/time.sol
@@ -1,19 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TimeOfDay {
-    uint mainfood;
-    function getTimeOfDay(uint256 hour) public pure returns (string memory) {
-        require(hour < 24, "Hour must be between 0 and 23");
+contract UserAuth {
+    struct User {
+        address walletAddress;
+        string username;
+        bool isRegistered;
+    }
 
-        if (hour >= 5 && hour < 12) {
-            return "Morning";
-        } else if (hour >= 12 && hour < 17) {
-            return "Afternoon";
-        } else if (hour >= 17 && hour < 21) {
-            return "Evening";
-        } else {
-            return "Night";
-        }
+    mapping(address => User) public users;
+
+    event UserRegistered(address userAddress, string username);
+
+    
+
+    function getUsername(address _user) public view returns (string memory) {
+        return users[_user].username;
     }
 }
