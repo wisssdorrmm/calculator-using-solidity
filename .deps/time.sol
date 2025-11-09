0edfff937a5ecc88bec95d89fe31d5f2f9eee137
wisssdorrmm <wisdomudoudo24@gmail.com>
2025-11-09 08:55:46 +0100

    new push

--- .deps/time.sol
+++ .deps/time.sol
@@ -12,7 +12,16 @@ contract UserAuth {
 
     event UserRegistered(address userAddress, string username);
 
-    
+    function registerUser(string memory _username) public {
+        require(!users[msg.sender].isRegistered, "User already registered");
+
+        users[msg.sender] = User(msg.sender, _username, true);
+        emit UserRegistered(msg.sender, _username);
+    }
+
+    function isUserRegistered(address _user) public view returns (bool) {
+        return users[_user].isRegistered;
+    }
 
     function getUsername(address _user) public view returns (string memory) {
         return users[_user].username;
